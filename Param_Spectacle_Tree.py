7ea5f355b469ec31c010e71eae8888c300eb36ec
Kevin Chen <kcchen@dls.rutgers.edu>
2015-10-07 10:41:35 -0400

    initial commit

---
+++ Param_Spectacle_Tree.py
@@ -0,0 +1,8 @@
+tree = {'GM12878':'H1-hESC','H1-hESC':'root','HepG2':'H1-hESC','HMEC':'H1-hESC','HSMM':'H1-hESC','HUVEC':'H1-hESC','K562':'H1-hESC','NHEK':'H1-hESC','NHLF':'H1-hESC'}
+
+marks = ['H3K27ac','H3K27me3','H3K36me3','H3K4me1','H3K4me2','H3K4me3','H3K9ac','H4K20me1']
+
+chromosomes = ['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX']
+
+inputdir = 'ENCODE'
+
